c6cc9f63e37ccabfa236d63281b8825a962d0ec4
David Gaskin <davidburkegaskin@gmail.com>
2019-08-12 23:05:50 -0700

    add robots.txt; add robots noindex meta to header

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="In floor heating offers many benefits. Learn about hydronic infloor heat from experts serving Toronto and Southern Ontario.">
+		<meta name="robots" content="noindex,nofollow">
 		<title>In-Floor Radiant Solutions</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/global.min.css" media="screen" title="Global Styles">
